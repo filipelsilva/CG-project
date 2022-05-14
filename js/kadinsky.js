08043882e1f71953aa8e2f7cebf0a9e381753788
ricardomartins <ricardo.martins.escola@gmail.com>
2022-05-14 16:48:05 +0100

    added more shapes

--- js/kadinsky.js
+++ js/kadinsky.js
@@ -7,14 +7,30 @@ function createObjects() {
 	material1 = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: false });
 	material2 = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: false });
 	material3 = new THREE.MeshBasicMaterial({ color: 0x0000ff, wireframe: false });
+	material4 = new THREE.MeshBasicMaterial({ color: 0xff00ff, wireframe: false });
+	material5 = new THREE.MeshBasicMaterial({ color: 0x24daf2, wireframe: false });
+	material6 = new THREE.MeshBasicMaterial({ color: 0xff7300, wireframe: false });
+	material7 = new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: false });
+	material8 = new THREE.MeshBasicMaterial({ color: 0x000000, wireframe: false });
 
 	geometry1 = new THREE.ConeGeometry(50,200,70);
 	geometry2 = new THREE.BoxGeometry(30,650,50);
 	geometry3 = new THREE.SphereGeometry(50, 20 ,20);
+	geometry4 = new THREE.ConeGeometry(50, 200, 4);
+	geometry5 = new THREE.CylinderGeometry(15, 15, 650, 50);
+	geometry6 = new THREE.BoxGeometry(125, 125, 125);
+	geometry7 = new THREE.TorusGeometry(100,10, 30, 30);
+	geometry8 = new THREE.BoxGeometry(30,690,50);
+
 
 	cone1 = new THREE.Mesh(geometry1, material1);
 	cube2 = new THREE.Mesh(geometry2, material2);
 	sphere3 = new THREE.Mesh(geometry3, material3);
+	pyramid4 = new THREE.Mesh(geometry4, material4);
+	cylinder5 = new THREE.Mesh(geometry5, material5);
+	cube6 = new THREE.Mesh(geometry6, material6);
+	torus7 = new THREE.Mesh(geometry7, material7);
+	cube8 = new THREE.Mesh(geometry8, material8);
 
 	cone1.position.set(-550, 250, -500);
 	cone1.rotation.z = -(Math.PI / 4) - (Math.PI /12);
@@ -22,13 +38,33 @@ function createObjects() {
 	cube2.position.set(-500, 0, -200);
 	cube2.rotation.z = Math.PI / 24;
 
-	sphere3.position.set(-530, 100, 0);
+	sphere3.position.set(-530, 120, 0);
 
+	pyramid4.position.set(-500, 0, -500);
+	pyramid4.rotation.z = (Math.PI / 4) * 2.5;
+	pyramid4.rotation.x =  Math.PI /50;
+
+	cylinder5.position.set(-320, -10, -100);
+	cylinder5.rotation.z = -(Math.PI / 4) * 1.4;
+
+	cube6.position.set(-480,-225,0);
+	cube6.rotation.y = -Math.PI / 8;
+	cube6.rotation.z = -Math.PI / 24;
+
+	torus7.position.set(-215, 215, 0);
+
+	cube8.position.set(-100, -10, 500);
+	cube8.rotation.z = Math.PI /2.8;
 
 	group = new THREE.Group();
 	group.add(cone1);
 	group.add(cube2);
 	group.add(sphere3);
+	group.add(pyramid4);
+	group.add(cylinder5);
+	group.add(cube6);
+	group.add(torus7);
+	group.add(cube8);
 
 	scene.add(group);
 }
