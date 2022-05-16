478a78b4f3f936b1b7775217749bd4b93adcbcba
ricardomartins <ricardo.martins.escola@gmail.com>
2022-05-16 10:00:19 +0100

    Refactored

--- js/kadinsky.js
+++ js/kadinsky.js
@@ -10,112 +10,112 @@ function createObjects() {
 	geometry = new THREE.ConeGeometry(50,200,70);
 	objects.push(new THREE.Mesh(geometry, material));
 
+	objects[0].position.set(-550, 250, -320);
+	objects[0].rotation.z = -(Math.PI / 4) - (Math.PI /12);
+
 	material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 	geometry = new THREE.BoxGeometry(30,650,50);
 	objects.push(new THREE.Mesh(geometry, material));
+
+	objects[1].position.set(-500, 0, -100);
+	objects[1].rotation.z = Math.PI / 24;
 
 	material = new THREE.MeshBasicMaterial({ color: 0x0000ff });
 	geometry = new THREE.SphereGeometry(50, 20 ,20);
 	objects.push(new THREE.Mesh(geometry, material));
 
+	objects[2].position.set(-530, 120, 100);
+
 	material = new THREE.MeshBasicMaterial({ color: 0xff00ff });
 	geometry = new THREE.ConeGeometry(50, 200, 4);
 	objects.push(new THREE.Mesh(geometry, material));
-
-	material = new THREE.MeshBasicMaterial({ color: 0x24daf2 });
-	geometry = new THREE.CylinderGeometry(15, 15, 650, 50);
-	objects.push(new THREE.Mesh(geometry, material));
-
-	material = new THREE.MeshBasicMaterial({ color: 0xff7300 });
-	geometry = new THREE.BoxGeometry(125, 125, 125);
-	objects.push(new THREE.Mesh(geometry, material));
-
-	material = new THREE.MeshBasicMaterial({ color: 0xffffff });
-	geometry = new THREE.TorusGeometry(100,10, 30, 30);
-	objects.push(new THREE.Mesh(geometry, material));
-
-	material = new THREE.MeshBasicMaterial({ color: 0x000000 });
-	geometry = new THREE.BoxGeometry(30,690,50);
-	objects.push(new THREE.Mesh(geometry, material));
-
-	material = new THREE.MeshBasicMaterial({ color: 0x45db00 });
-	geometry = new THREE.BoxGeometry(130,130,130);
-	objects.push(new THREE.Mesh(geometry, material));
-
-	material = new THREE.MeshBasicMaterial({ color: 0xfc8a8a });
-	geometry = new THREE.CylinderGeometry(15, 15, 900, 50);
-	objects.push(new THREE.Mesh(geometry, material));
-
-	material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-	geometry = new THREE.SphereGeometry(100, 20, 20);
-	objects.push(new THREE.Mesh(geometry, material));
-
-	material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-	geometry = new THREE.ConeGeometry(125, 350, 4);
-	objects.push(new THREE.Mesh(geometry, material));
-
-	material = new THREE.MeshBasicMaterial({ color: 0x000b70 });
-	geometry = new THREE.SphereGeometry(150, 20, 20);
-	objects.push(new THREE.Mesh(geometry, material));
-
-	material = new THREE.MeshBasicMaterial({ color: 0xddff00 });
-	geometry = new THREE.TorusGeometry(175, 12, 30, 30);
-	objects.push(new THREE.Mesh(geometry, material));
-
-	material = new THREE.MeshBasicMaterial({ color: 0x9500ff });
-	geometry = new THREE.ConeGeometry(50,200,70);
-	objects.push(new THREE.Mesh(geometry, material));
-
-	material = new THREE.MeshBasicMaterial({ color: 0xa14e00 });
-	geometry = new THREE.BoxGeometry(125,125,125);
-	objects.push(new THREE.Mesh(geometry, material));
-
-	objects[0].position.set(-550, 250, -320);
-	objects[0].rotation.z = -(Math.PI / 4) - (Math.PI /12);
-
-	objects[1].position.set(-500, 0, -100);
-	objects[1].rotation.z = Math.PI / 24;
-
-	objects[2].position.set(-530, 120, 100);
 
 	objects[3].position.set(-500, 0, -320);
 	objects[3].rotation.z = (Math.PI / 4) * 2.5;
 	objects[3].rotation.x =  Math.PI /50;
 
+	material = new THREE.MeshBasicMaterial({ color: 0x24daf2 });
+	geometry = new THREE.CylinderGeometry(15, 15, 650, 50);
+	objects.push(new THREE.Mesh(geometry, material));
+
 	objects[4].position.set(-320, -10, -200);
 	objects[4].rotation.z = -(Math.PI / 4) * 1.4;
+
+	material = new THREE.MeshBasicMaterial({ color: 0xff7300 });
+	geometry = new THREE.BoxGeometry(125, 125, 125);
+	objects.push(new THREE.Mesh(geometry, material));
 
 	objects[5].position.set(-480,-225,150);
 	objects[5].rotation.y = -Math.PI / 8;
 	objects[5].rotation.z = -Math.PI / 24;
 
+	material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+	geometry = new THREE.TorusGeometry(100,10, 30, 30);
+	objects.push(new THREE.Mesh(geometry, material));
+
 	objects[6].position.set(-215, 215, 0);
+
+	material = new THREE.MeshBasicMaterial({ color: 0x000000 });
+	geometry = new THREE.BoxGeometry(30,690,50);
+	objects.push(new THREE.Mesh(geometry, material));
 
 	objects[7].position.set(-100, -10, 680);
 	objects[7].rotation.z = Math.PI /2.8;
+
+	material = new THREE.MeshBasicMaterial({ color: 0x45db00 });
+	geometry = new THREE.BoxGeometry(130,130,130);
+	objects.push(new THREE.Mesh(geometry, material));
 
 	objects[8].position.set(-125, -200, -500);
 	objects[8].rotation.z = Math.PI /4;
 	objects[8].rotation.x = Math.PI /12;
 
+	material = new THREE.MeshBasicMaterial({ color: 0xfc8a8a });
+	geometry = new THREE.CylinderGeometry(15, 15, 900, 50);
+	objects.push(new THREE.Mesh(geometry, material));
+
 	objects[9].position.set(225, -70, 680);
 	objects[9].rotation.z = Math.PI / 2;
 
+	material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+	geometry = new THREE.SphereGeometry(100, 20, 20);
+	objects.push(new THREE.Mesh(geometry, material));
+
 	objects[10].position.set(350, -225, 450);
+
+	material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+	geometry = new THREE.ConeGeometry(125, 350, 4);
+	objects.push(new THREE.Mesh(geometry, material));
 
 	objects[11].position.set(550, -80, 580);
 	objects[11].rotation.z = -Math.PI/1.05 ;
 	objects[11].rotation.y = Math.PI/24 ;
 
+	material = new THREE.MeshBasicMaterial({ color: 0x000b70 });
+	geometry = new THREE.SphereGeometry(150, 20, 20);
+	objects.push(new THREE.Mesh(geometry, material));
+
 	objects[12].position.set(350, 150, -550);
+
+	material = new THREE.MeshBasicMaterial({ color: 0xddff00 });
+	geometry = new THREE.TorusGeometry(175, 12, 30, 30);
+	objects.push(new THREE.Mesh(geometry, material));
 
 	objects[13].position.set(350, 150, -550)
 	objects[13].rotation.y = -Math.PI / 4;
 	objects[13].rotation.x = -Math.PI /2.5;
 	objects[13].rotation.z = Math.PI;
 
+	material = new THREE.MeshBasicMaterial({ color: 0x9500ff });
+	geometry = new THREE.ConeGeometry(50,200,70);
+	objects.push(new THREE.Mesh(geometry, material));
+
 	objects[14].position.set(75, 75, 200);
 	objects[14].rotation.z = -Math.PI /3.7;
+
+	material = new THREE.MeshBasicMaterial({ color: 0xa14e00 });
+	geometry = new THREE.BoxGeometry(125,125,125);
+	objects.push(new THREE.Mesh(geometry, material));
 
 	objects[15].position.set(50, 230, 400);
 	objects[15].rotation.z = -Math.PI/4;
