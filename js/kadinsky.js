49b8a3a2f8b07d42f712519c7fc2fcf6abdf17d3
André Esgalhado <andre.esgalhado@tecnico.ulisboa.pt>
2022-05-20 02:14:58 +0100

    lights and idk

--- js/kadinsky.js
+++ js/kadinsky.js
@@ -48,104 +48,108 @@ function createObjects() {
 	let objects = [];
 	group = new THREE.Group();
 
-	material = new THREE.MeshBasicMaterial({ color: 0xffb700 });
-	geometry = new THREE.BoxGeometry(110,110,110);
+	//avô
+	material = new THREE.MeshStandardMaterial({ color: 0xffb700 });
+	geometry = new THREE.BoxGeometry(110, 110, 110);
 	objects.push(new THREE.Mesh(geometry, material));
 
 	objects[0].position.set(0, 0, 0);
 	objects[0].rotation.z = Math.PI /4;
 	objects[0].rotation.x = Math.PI /4;
 
-	material = new THREE.MeshBasicMaterial({ color: 0xffffff });
-	geometry = new THREE.TorusGeometry(120,11, 30, 30);
+	//avó
+	material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+	geometry = new THREE.TorusGeometry(120, 11, 30, 30);
 	objects.push(new THREE.Mesh(geometry, material));
 
 	objects[1].position.set(0, 0, 0);
 	objects[1].rotation.x = -Math.PI /2.5;
 	objects[1].rotation.y = Math.PI /4;
 
-	material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+	material = new THREE.MeshStandardMaterial({ color: 0xff0000 });
 	geometry = new THREE.OctahedronGeometry(120, 0);
 	objects.push(new THREE.Mesh(geometry, material));
 
 	objects[2].position.set(-400, -170, 100);
 	objects[2].rotation.z = Math.PI /3;
 
-	material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+	//pai
+	material = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
 	geometry = new THREE.SphereGeometry(60, 20 ,20);
 	objects.push(new THREE.Mesh(geometry, material));
 
 	objects[3].position.set(150, -210, 100);
 
-	material = new THREE.MeshBasicMaterial({ color: 0xfffb00 });
+	material = new THREE.MeshStandardMaterial({ color: 0xfffb00 });
 	geometry = new THREE.SphereGeometry(75, 20 ,20);
 	objects.push(new THREE.Mesh(geometry, material));
 
 	objects[4].position.set(100, 220, 40);
 
-	material = new THREE.MeshBasicMaterial({ color: 0x00ffff });
+	material = new THREE.MeshStandardMaterial({ color: 0x00ffff });
 	geometry = new THREE.IcosahedronGeometry(120, 0);
 	objects.push(new THREE.Mesh(geometry, material));
 
 	objects[5].position.set(350, 100, 100);
 	//objects[5].rotation.z = Math.PI /3;
 
-	material = new THREE.MeshBasicMaterial({ color: 0xfc01a8 });
+	material = new THREE.MeshStandardMaterial({ color: 0xfc01a8 });
 	geometry = new THREE.SphereGeometry(35, 20 ,20);
 	objects.push(new THREE.Mesh(geometry, material));
 
 	objects[6].position.set(-100, -220, 30);
 
-	material = new THREE.MeshBasicMaterial({ color: 0x82877e });
+	material = new THREE.MeshStandardMaterial({ color: 0x82877e });
 	geometry = new THREE.SphereGeometry(110, 20 ,20);
 	objects.push(new THREE.Mesh(geometry, material));
 
 	objects[7].position.set(-330, 130, -250);
 
-	material = new THREE.MeshBasicMaterial({ color: 0xff2f00 });
+	material = new THREE.MeshStandardMaterial({ color: 0xff2f00 });
 	geometry = new THREE.CylinderGeometry(10, 10, 650, 50);
 	objects.push(new THREE.Mesh(geometry, material));
 
 	objects[8].position.set(350, -270, -100);
-	objects[8].rotation.z = -(Math.PI / 2.2) ;
+	objects[8].rotation.z = -(Math.PI / 2.2);
 
-	material = new THREE.MeshBasicMaterial({ color: 0x01fc93 });
+	material = new THREE.MeshStandardMaterial({ color: 0x01fc93 });
 	geometry = new THREE.CylinderGeometry(25, 25, 650, 50);
 	objects.push(new THREE.Mesh(geometry, material));
 
 	objects[9].position.set(600, 0, -250);
-	objects[9].rotation.z = -(Math.PI / 25) ;
+	objects[9].rotation.z = -(Math.PI / 25);
 
-	material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+	material = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
 	geometry = new THREE.ConeGeometry(45, 150, 4);
 	objects.push(new THREE.Mesh(geometry, material));
 
 	objects[10].position.set(-420, 75, -110);
 	objects[10].rotation.z = -1.2*(Math.PI / 4) ;
 
-	material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+	//neto
+	material = new THREE.MeshStandardMaterial({ color: 0xff0000 });
 	geometry = new THREE.ConeGeometry(25, 100, 50);
 	objects.push(new THREE.Mesh(geometry, material));
 
 	objects[11].position.set(240, -200, 100);
 	objects[11].rotation.z =Math.PI/30;
 
-	material = new THREE.MeshBasicMaterial({ color: 0xff008c });
+	material = new THREE.MeshStandardMaterial({ color: 0xff008c });
 	path = new CustomSinCurve( 250 );
 	geometry = new THREE.TubeGeometry(path, 30, 8, 13);
 	objects.push(new THREE.Mesh(geometry, material));
 
 	objects[12].position.set(-365, 25, 0);
 	objects[12].rotation.z = 0.7*Math.PI/4
-
-	material = new THREE.MeshBasicMaterial({ color: 0x6057ff });
+	
+	material = new THREE.MeshStandardMaterial({ color: 0x6057ff });
 	geometry = new THREE.CylinderGeometry(70, 30, 120, 13);
 	objects.push(new THREE.Mesh(geometry, material));
 
 	objects[13].position.set(-180, -100, 0);
 	objects[13].rotation.z = -1.2*Math.PI/6
 
-	material = new THREE.MeshBasicMaterial({ color: 0xff57cd });
+	material = new THREE.MeshStandardMaterial({ color: 0xff57cd });
 	geometry = new THREE.OctahedronGeometry(70, 0);
 	objects.push(new THREE.Mesh(geometry, material));
 
@@ -153,7 +157,7 @@ function createObjects() {
 	objects[14].rotation.z = -1.6*Math.PI /3;
 	objects[14].rotation.z = -1.6*Math.PI /2;
 
-	material = new THREE.MeshBasicMaterial({ color: 0xc1c1c1 });
+	material = new THREE.MeshStandardMaterial({ color: 0xc1c1c1 });
 	geometry = new THREE.BoxGeometry(80, 80, 80 );
 	objects.push(new THREE.Mesh(geometry, material));
 
@@ -179,6 +183,15 @@ function createScene() {
 	scene = new THREE.Scene();
 	scene.background = new THREE.Color(0x020122);
 	scene.add(axes);
+
+	const light1 = new THREE.DirectionalLight( 0xffffff, 1 );
+	light1.position.set(1000, 1000, 1000);
+	light1.castShadow = true;
+	scene.add(light1);
+	const light2 = new THREE.DirectionalLight( 0xffffff, 1 );
+	light2.position.set(-1000, 1000, -1000);
+	light2.castShadow = true;
+	scene.add(light2);
 }
 
 function createCamera() {
@@ -196,6 +209,7 @@ function doKeyPress(delta) {
 	if (keyMap[81] || keyMap[113]) { // Q/q
 		// rodar o objeto todo para um lado
 		articulate.rotation.z += 0.001 * delta;
+		//articulate.rotateX(Math.PI * 0.001 * delta)
 	}
 
 	if (keyMap[87] || keyMap[119]) { // W/w
@@ -228,7 +242,7 @@ function doKeyPress(delta) {
 		rotateAroundPoint(
 			articulate.children[0].children[0],
 			getObjectCenterPoint(articulate.children[0]),
-			new THREE.Vector3(0, 1, 0),
+			new THREE.Vector3(0, 0, 1),
 			0.001 * delta
 		);
 	}
@@ -238,7 +252,7 @@ function doKeyPress(delta) {
 		rotateAroundPoint(
 			articulate.children[0].children[0],
 			getObjectCenterPoint(articulate.children[0]),
-			new THREE.Vector3(0, 1, 0),
+			new THREE.Vector3(0, 0, 1),
 			-0.001 * delta
 		);
 	}
@@ -269,30 +283,44 @@ function doKeyPress(delta) {
 }
 
 function doOneTimeEvent(code) {
+	var flag = false;
 	switch (code) {
 		case 69:  // E
 		case 101: // e
+			flag = true;
 			axes.visible = !axes.visible;
 			break;
 		case 49: // 1
+			flag = true;
 			camera.position.x = 0;
 			camera.position.y = 0;
 			camera.position.z = 1;
 			camera.lookAt(0, 0, 0);
 			break;
 		case 50: // 2
+			flag = true;
 			camera.position.x = 0;
 			camera.position.y = 1;
 			camera.position.z = 0;
 			camera.lookAt(0, 0, 0);
 			break;
 		case 51: // 3
+			flag = true;
 			camera.position.x = 1;
 			camera.position.y = 0;
 			camera.position.z = 0;
 			camera.lookAt(0, 0, 0);
 			break;
 		case 52: // 4
+			flag = true;
+			function changeWireframe(listMesh) {
+				listMesh.children.map(child => {
+					child.material.wireframe = !child.material.wireframe;
+					if (child.children.length != 0) {
+						changeWireframe(child);
+					}
+				});
+			}
 			changeWireframe(group);
 			break;
 		default:
