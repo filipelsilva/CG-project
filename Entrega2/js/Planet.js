be334765c72b9878d47ae4a59e8c81689b8a639c
ricardomartins <ricardo.martins.escola@gmail.com>
2022-06-01 19:24:59 +0100

    Fixed images

--- Entrega2/js/Planet.js
+++ Entrega2/js/Planet.js
@@ -1,7 +1,7 @@
 class Planet {
 	constructor(R) {
 		//let material = new THREE.MeshStandardMaterial({ color: 0x01fc93 });
-		material = new THREE.MeshStandardMaterial({map : new THREE.TextureLoader().load( 'js/textures/earth.jpg' )});
+		let material = new THREE.MeshStandardMaterial({map : new THREE.TextureLoader().load( 'js/textures/earth.jpg' )});
 		let geometry = new THREE.SphereGeometry(R, 32, 16);
 		this.planet = new THREE.Mesh(geometry, material);
 		this.planet.position.set(0, 0, 0);
