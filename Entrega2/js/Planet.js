845d40e2031125e0b0055d2ec5e08067aacf4ae8
ricardomartins <ricardo.martins.escola@gmail.com>
2022-06-01 19:33:52 +0100

    Rotated planet

--- Entrega2/js/Planet.js
+++ Entrega2/js/Planet.js
@@ -1,10 +1,13 @@
 class Planet {
 	constructor(R) {
 		//let material = new THREE.MeshStandardMaterial({ color: 0x01fc93 });
-		let material = new THREE.MeshStandardMaterial({map : new THREE.TextureLoader().load( 'js/textures/earth.jpg' )});
+		const texture = new THREE.TextureLoader().load( "js/textures/earth.jpg" );
+		let material = new THREE.MeshStandardMaterial({map : texture });
 		let geometry = new THREE.SphereGeometry(R, 32, 16);
 		this.planet = new THREE.Mesh(geometry, material);
 		this.planet.position.set(0, 0, 0);
+		this.planet.rotation.y = -Math.PI/2 - 0.9;
+		this.planet.rotation.x = Math.PI/16;
 	}
 
 	getPlanet(){
