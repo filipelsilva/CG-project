eae6595b080305c06c8320a0601e123e08527dda
André Esgalhado <andre.esgalhado@tecnico.ulisboa.pt>
2022-06-17 22:57:10 +0100

    final delivery

---
+++ Entrega3/deliver.sh
@@ -0,0 +1,3 @@
+#/bin/env bash
+rm -f G40-Alameda.zip
+zip -r G40-Alameda.zip Entrega3.pdf index.html js/three.js js/main.js js/KeyHandler.js js/ObjectCreator.js js/SceneCreator.js js/VRButton.js media/pause_menu.png media/origami.jpg
